28caae90e6742d47d35bd5eb348148cf2665446b
Ivan Almada <ivan.almada@me.com>
2024-02-27 16:31:15 -0600

    Initial commit

---
+++ pattern-invert-swiftui/pattern_invert_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  pattern_invert_swiftuiApp.swift
+//  pattern-invert-swiftui
+//
+//  Created by Iván Almada on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct pattern_invert_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
