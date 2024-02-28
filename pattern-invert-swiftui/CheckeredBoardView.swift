6e4aea5bc3d9c56e20456dac6a33e2cda418b757
Ivan Almada <ivan.almada@me.com>
2024-02-27 19:18:31 -0600

    Add parameter for the checkeredboard to start black or white

--- pattern-invert-swiftui/CheckeredBoardView.swift
+++ pattern-invert-swiftui/CheckeredBoardView.swift
@@ -8,20 +8,37 @@
 import SwiftUI
 
 struct CheckeredBoardView: View {
+
+    @State var startsBlack: Bool
+
     var body: some View {
-        VStack(spacing: 0) {
-            CheckeredRowView(startsBlack: true)
-            CheckeredRowView(startsBlack: false)
-            CheckeredRowView(startsBlack: true)
-            CheckeredRowView(startsBlack: false)
-            CheckeredRowView(startsBlack: true)
-            CheckeredRowView(startsBlack: false)
-            CheckeredRowView(startsBlack: true)
-            CheckeredRowView(startsBlack: false)
+        if startsBlack {
+            VStack(spacing: 0) {
+                CheckeredRowView(startsBlack: true)
+                CheckeredRowView(startsBlack: false)
+                CheckeredRowView(startsBlack: true)
+                CheckeredRowView(startsBlack: false)
+                CheckeredRowView(startsBlack: true)
+                CheckeredRowView(startsBlack: false)
+                CheckeredRowView(startsBlack: true)
+                CheckeredRowView(startsBlack: false)
+            }
+        } else {
+            VStack(spacing: 0) {
+                CheckeredRowView(startsBlack: false)
+                CheckeredRowView(startsBlack: true)
+                CheckeredRowView(startsBlack: false)
+                CheckeredRowView(startsBlack: true)
+                CheckeredRowView(startsBlack: false)
+                CheckeredRowView(startsBlack: true)
+                CheckeredRowView(startsBlack: false)
+                CheckeredRowView(startsBlack: true)
+            }
         }
+
     }
 }
 
 #Preview {
-    CheckeredBoardView()
+    CheckeredBoardView(startsBlack: false)
 }
